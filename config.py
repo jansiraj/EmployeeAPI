05019383c096f29adcf3c84a1089321224c9c516
jansiraj <116732511+jansiraj@users.noreply.github.com>
2024-08-09 12:30:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ class Config(object):
     SECRET_KEY = 'GDtfDCFYjD'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///company.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    AWS_ACCESS_KEY_ID: 'ABCDEFGHIJKLMNOPQRST'
